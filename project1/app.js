93c12b8b49d204c72199f93d82d99ae6c131e761
Nathaniel Okun <nokun@stanford.edu>
2014-11-05 11:29:41 -0800

    Working on MVC view. Able to render first two pages. Working on using post requests when a button is pressed or a form is submitted.

--- project1/app.js
+++ project1/app.js
@@ -1,20 +1,40 @@
 var express = require('express');
 var path = require('path');
+var redis = require('redis');
+var session = require('express-session')
+var cookieParser = require('cookie-parser');
+
+var client = redis.createClient(6379, 'localhost');
+
+
+
 // var favicon = require('serve-favicon');
 // var logger = require('morgan');
 // var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
 
-//New Code
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 //var questions = require('public/questions')
 
 var app = express();
+
+
+app.use(session({
+    secret: 'a4f8071f-c873-4447-8ee2',
+    cookie: { maxAge: 2628000000 },
+    store: new (require('express-sessions'))({
+        storage: 'redis',
+        instance: client, // optional
+        host: 'localhost', // optional
+        port: 6379, // optional
+        collection: 'sessions', // optional
+        expire: 86400 // optional
+    })
+}));
 
 // view engine setup
 app.listen('3000');
@@ -28,40 +48,46 @@ app.set('view options', { layout:'layout.ejs' });
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(cookieParser());
+
+//Allows us to us req.session
+app.use(cookieParser());
+
+//What does this do?
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(expressLayouts);
 app.use('/', routes);
 
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-// // error handlers
+// error handlers
 
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//     app.use(function(err, req, res, next) {
-//         res.status(err.status || 500);
-//         res.render('error', {
-//             message: err.message,
-//             error: err
-//         });
-//     });
-// }
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//         message: err.message,
-//         error: {}
-//     });
-// });
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 
 
--- project1/public/javascripts/introduction-view.js
+++ project1/public/javascripts/introduction-view.js
@@ -17,82 +17,11 @@ function addOpeningScreenButtonListener() {
 			event.preventDefault();
 			return;
 		} else if (event.target.id === 'choose-spanish') {
-			console.log('spanish');
-			renderNextScreen('spanish');
+			//NOTHING FOR NOW
 		} else if (event.target.id === 'choose-english') {
-			console.log('english');
-			renderNextScreen('english');
+			//NORTHING FOR NOW
 		}
 	});
 }
-
-function renderNextScreen(language) {
-	$.get('/' + language, {
-		line1: 'Welcome! You are about to take the exam portion of the United States Citizenship Test.',
-		line2: 'The exam is 100 questions long. Right now, you may take all 100 questions, 50 questions or 20 questions.',
-		line3: 'Please choose below.',
-		line4: 'Or, type the number of questions you want to answer below:'
-	}, function(data) {
-		console.log('hi', data);
-	}, 'json');
-}
-
-/**
-* Creates a welcome page. Shrinks the title of page and allows user to choice how 
-* many questions they would like to see in the subsequent session.
-* Also adds button listeners to detect how many questions are going to be used.
-*/
-function createWelcomePage(language) {
-
-	introduceTest(language);
-	addTestSpecificationsListeners(language);
-}
-
-function addTestSpecificationsListeners(language) {
-	$('#main-page').click(function(event) {
-		if(event.target.nodeName != 'BUTTON') {
-			event.preventDefault();
-			return;
-		} else {
-			var numQuestions = event.target.firstChild.nodeValue;
-			TestTakingView.startTest(numQuestions, language);
-		}
-	});
-
-	$('#main-page').submit(function(event) {
-		event.preventDefault();
-		if(event.target.id === 'numquestions') {
-			var numQuestions = $(event.target).find('[name="number"]').val();
-			//Implement not a number functionality
-			TestTakingView.startTest(numQuestions, language);
-		}
-	});
-}
-
-/**
-* Instatiates introduction to the test using handlebars.
-*
-*/
-function introduceTest(language) {
-	if(language === 'spanish') {
-		var welcome = 'Bienvenidos! Este es el la porción de examen del examen para la ciudadanía estadounidense.';
-		var text1 = 'El examen es 100 preguntas. Ahora, puede elegir tomar todo, 50, o 20 preguntas.';
-		var text2 = 'Elige abajo.';
-		var text3 = 'O, tipa el numero de preguntas a que quiere responder:';
-	} else {
-		var welcome = 'Welcome! You are about to take the exam portion of the United States Citizenship Test.';
-		var text1 = 'The exam is 100 questions long. Right now, you may take all 100 questions, 50 questions or 20 questions.';
-		var text2 = 'Please choose below.';
-		var text3 = 'Or, type the number of questions you want to answer below:';
-
-	}
-
-	var introduction = {line1: welcome, line2: text1, line3: text2, line4: text3};
-	var intro = handlebarsTemplates.renderIntroForm(introduction);
-
-	$openingScreen.html(intro);
-}
-
-
 
 })(this, this.document);
